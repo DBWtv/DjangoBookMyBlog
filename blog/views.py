18138526338b5ece7cc2d7a79bb039cbd9af52bc
DBWtv <dima.bwc@gmail.com>
2022-12-09 16:47:08 +0600

    working (local) mail send

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-
+    
 # def post_list(request):
 #     object_list = Post.published.all()
 #     paginator = Paginator(object_list, 3)
@@ -28,11 +29,17 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{}({}) recommends you reading"{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'dima.bwc@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
