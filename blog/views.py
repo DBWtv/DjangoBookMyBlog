03180ff38fbffbc2d50859b9557378b29c920761
DBWtv <dima.bwc@gmail.com>
2022-12-12 17:46:28 +0600

    working with search

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 # class PostListView(ListView):
 #     queryset = Post.published.all()
@@ -17,18 +17,30 @@ from django.contrib.postgres.search import SearchVector
 def post_search(request):
     form = SearchForm()
     query = None
-    result = []
+    results = []
     if 'query' in request.GET:
         form = SearchForm(request.GET)
     if form.is_valid():
         query = form.cleaned_data['query']
+        search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+        search_query = SearchQuery(query)
+
+        # results = Post.objects.annotate(
+        #     similarity=TrigramSimilarity('title', query),
+        #     ).filter(similarity__gt=0.3).order_by('-similarity')
+        # 
+        # 
+        # does not append 'CREATE EXTENSION pg_trgm'
+
         results = Post.objects.annotate(
-            search=SearchVector('title', 'body'),
-        ).filter(search=query)
+            search = search_vector,
+            rank = SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
     return render(request, 'blog/post/search.html', {
         'form': form,
         'query': query,
-        'result': result
+        'results': results
     })
     
 def post_list(request, tag_slug=None):
