e614324ea39a6a174e72e4c451e9c75014a9278a
DBWtv <dima.bwc@gmail.com>
2022-12-12 16:26:40 +0600

    add postgreSQL

---
+++ mysite/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
